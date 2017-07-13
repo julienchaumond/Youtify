2535d1c4ce3957cb202e859e0f689059408d1c20
Julien Chaumond <julien.chaumond@orange.com>
2017-07-13 15:50:18 +0200

    Init

---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+var mb = require('musicbrainz');
+var $ = require("jquery");
+/*
+mb.searchArtists("The XX", {}, function(err, artists) {
+	console.log(artists)
+})
+*/
+function search() {
+	console.log("fsfsefes");
+	var searchField = $("#searchField");
+	console.log(searchField);
+}
+
+function getArtist(artistName) {
+	console.log(artistName);
+}
